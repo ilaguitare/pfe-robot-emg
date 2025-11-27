db8500f9db174f06ea6fda177210f8b654a229db
ilaguitare <ithar.kazem@telecom-sudparis.eu>
2025-11-27 16:03:54 +0100

    it works pretty well

--- emg/2e essai/sketch_nov14b/sketch_nov14b.ino
+++ emg/2e essai/sketch_nov14b/sketch_nov14b.ino
@@ -4,8 +4,9 @@
 #include <BLEClient.h>
 #include <BLE2902.h>
 
-#define THRESHOLD 150
+#define THRESHOLD 100
 #define EMG_PIN 0
+#define N 16
 
 // Adresse MAC (voir la doc BLE ESP32 pour l'utilisation)
 static BLEAddress robotAddress("24:EC:4A:CE:3F:D6");
@@ -15,13 +16,14 @@ static BLEUUID charUUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
 BLEClient* pClient;
 BLERemoteCharacteristic* pRemoteCharacteristic;
 
-int prevVal = 0;
-int window[10];
+int buffer[N];
 int idx = 0;
+int prevVal = 0;
 
 void setup() {
   Serial.begin(115200);
-  // Initialisation BLE en "Client"
+  for(int i=0; i<N; i++) buffer[i]=0;
+    // Initialisation BLE en "Client"
   BLEDevice::init("");
   pClient = BLEDevice::createClient();
   bool connected = pClient->connect(robotAddress);
@@ -32,16 +34,24 @@ void setup() {
 }
 
 void loop() {
-  int val = analogRead(0);
-  int filtered = val - prevVal; // filtre passe-haut très simple
-  // Détection de bosse :
-  if (filtered > THRESHOLD) {
+  int val = analogRead(EMG_PIN);
+  int filtered = val - prevVal;
+  prevVal = val;
+  int envelope = abs(filtered); // ou filtered*filtered
+  buffer[idx] = envelope;
+  idx = (idx + 1) % N;
+  long sum = 0;
+  for(int i=0; i<N; i++) sum += buffer[i];
+  int smoothEnvelope = sum / N;
+  //Serial.print(val); Serial.print(",");
+  //Serial.print(filtered); Serial.print(",");
+
+  if (smoothEnvelope > THRESHOLD) {
     // Ici tu déclenches le robot
     // digitalWrite(moteurPin, HIGH);
     pRemoteCharacteristic->writeValue("F"); // envoie avance robot
     Serial.println("Contraction! Commande envoyée");
   }
-  prevVal = val;
-  Serial.println(filtered);
-  delay(100);
+  Serial.println(smoothEnvelope);
+  delay(30);
 }
